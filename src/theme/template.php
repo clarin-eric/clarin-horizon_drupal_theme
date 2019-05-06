4da06090015da47df5d8fddc4574c0129d458b4e
andmor- <amoreira.mail@gmail.com>
2019-05-06 18:50:53 +0200

    Removed fetching fonts from: fonts.googleapis.com

--- src/theme/template.php
+++ src/theme/template.php
@@ -8,36 +8,6 @@
  * Overrides theme_preprocess_html().
  */
 function CLARIN_Horizon_preprocess_html(&$vars) {
-  $fontCssLinkSourceSansPro = array(
-    '#type' => 'html_tag',
-    '#tag' => 'link',
-    '#attributes' => array(
-      'href' =>  'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,700i&amp;subset=latin-ext,vietnamese',
-      'rel' => 'stylesheet',
-      'type' => 'text/css'
-    )
-  );
-  
-  $fontCssLinkRobotoSlab = array(
-    '#type' => 'html_tag',
-    '#tag' => 'link',
-    '#attributes' => array(
-      'href' =>  'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese',
-      'rel' => 'stylesheet',
-      'type' => 'text/css'
-    )
-  );  
-  
-  $fontCssLinkSourceCodePro = array(
-    '#type' => 'html_tag',
-    '#tag' => 'link',
-    '#attributes' => array(
-      'href' =>  'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700&amp;subset=latin-ext',
-      'rel' => 'stylesheet',
-      'type' => 'text/css'
-    )
-  );
-	
   $compatibilityModeOff = array(
     '#type' => 'html_tag',
     '#tag' => 'meta',
@@ -48,9 +18,6 @@ function CLARIN_Horizon_preprocess_html(&$vars) {
   );
   
   // Add header meta tag for IE to head
-  drupal_add_html_head($fontCssLinkSourceSansPro, 'fontCssLinkSourceSansPro');
-  drupal_add_html_head($fontCssLinkRobotoSlab, 'fontCssLinkRobotoSlab');
-  drupal_add_html_head($fontCssLinkRobotoSlab, 'fontCssLinkSourceCodePro');
   drupal_add_html_head($compatibilityModeOff, 'compatibilityModeOff');
 }
 

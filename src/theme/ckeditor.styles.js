30a0d65fcea0c9ed52d2eac274a142838d9789f6
andmor <amoreira.mail@gmail.com>
2020-03-26 10:54:01 +0100

    Change style to be used with the regular image plugin

--- src/theme/ckeditor.styles.js
+++ src/theme/ckeditor.styles.js
@@ -6,6 +6,6 @@ see: /var/www/localhost/htdocs/sites/all/libraries/ckeditor/styles.js
 if(typeof(CKEDITOR) !== 'undefined') {
     CKEDITOR.addStylesSet( 'drupal',
     [
-            { name: 'Image Responsive', type: 'widget', widget: 'image', attributes: { 'class': 'img-responsive' } }
+            { name: 'Image Responsive', element: 'img', attributes: { 'class': 'img-responsive' } }
     ]);
 }
